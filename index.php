89d2207df05e146ffb6e70fbef779126d595bde8
proyectoaircomment <proyecto.aircomment@gmail.com>
2016-03-08 00:40:06 +0000

    Primera version del algoritmo para hacer consultas a paginas web. Commit 3

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ echo "mensaje 1";
 $link = "http://190.27.249.245/obs";
 
 
-//por ahora no sirve
+//Obtiene el contenido de una et
 function get_content($url,$tag)
 {
     $html = file_get_html($url);
@@ -25,27 +25,49 @@ function get_content($url,$tag)
      }
 }
 
-function getArray($array)
+function printArray($array)
 {
-    $i=0;
+    //$i=0;
     foreach ($array as $value) 
     {
-        $i++;
-        echo " [".$i."]: ".$value;
+        //$i++;
+        //echo " [".$i."]: ".$value;
+        echo $value;
+        echo "<br>";
     }
 }
 
+function divideString($array)
+{
+    global $estacion, $mensaje;
+    foreach($array as $row)
+    {
+        $long = strlen ( $row );
+        $estacion[] = substr($row,11,4);
+        $mensaje[] = substr($row,16,$long-43);
+    }
+}
+
+
+echo "Impresion de todo el contenido...<br>";
 get_content($link, 'div',$metars);
 
 echo "<br>";
 
-echo count($metars);
+echo "Numero de elementos del array metars ".count($metars);
 echo "<p>el array es: </p>";
+printArray($metars);
 
-getArray($metars);
+echo "<br>";
+//echo pasarAjson($metars);
+divideString($metars);
 
-//get_content($link, 'table');
+echo "Impresion de las estaciones...<br>";
+printArray($estacion);
 
+echo "<br>";
+echo "Impresion de los mensajes..<br>";
+printArray($mensaje);
 
 ?>
 
